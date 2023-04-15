272e1098042ba6923380ec7c7f0e8e94637d2557
Luiz <l.bueno@xsensors.ai>
2023-04-15 20:56:32 -0300

    creation of infor row

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,29 @@
 import { Header } from "./Header";
-import "./global.css";
+
+import styles from "./App.module.css";
+import { useState } from "react";
 
 export function App() {
+  const [tasks, setTasks] = useState([]);
   return (
-    <div className="App">
+    <div className={styles.App}>
       <Header />
+
+      <div className={styles.body}>
+        <div className={styles["info-row"]}>
+          <div className="created">
+            Created Tasks
+            <span className={styles.badge}>{tasks.length}</span>
+          </div>
+
+          <div className="done-section">
+            Done{" "}
+            <span className={styles.badge}>
+              {tasks.length ? `2 of ${tasks.length}` : 0}
+            </span>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
